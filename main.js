196eaabac33361d7a714662d082148e55b86fa7a
chiquang98 <chiquang1498@gmail.com>
2021-07-06 14:19:20 +0700

    Q

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 var http = require('http');
-var express = require("express");
+
 var RED = require("node-red");
+var cors = require('cors')
 const fs = require('fs')
+var express = require("express");
 const session = require('express-session');
+
+var livereload  = require("connect-livereload");
 // Create an Express app
 var app = express();
 //session
+app.use(cors())
 app.use(session({
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     secret: 'somesecret',
-    cookie: { maxAge: 6000000 }}));
+    cookie: {
+        maxAge: 365 * 24 * 60 * 60 * 1000
+    }
+})
+);
+
 // Add a simple route for static content served from 'public'
 app.use("/",express.static("public"));
-
+app.enable('trust proxy');
 // Create a server
 var server = http.createServer(app);
 
@@ -23,21 +33,11 @@ var settings = {
     httpNodeRoot: "/api",
     userDir:"/home/nol/.nodered/",
     functionGlobalContext: { } ,
-    // storageModule : require("node-red-mongo-storage-plugin"),
-    // storageModuleOptions: {
-    //     mongoUrl: 'mongodb://192.168.245.128:27017',
-    //     //also possible
-    //     //mongoUrl: 'PROCESS.ENV.MONGOURL',
-    //     database: 'local',
-    //     //optional
-    //     //set the collection name that the module would be using
-    //     collectionNames:{
-    //         flows: "nodered-flows",
-    //         credentials: "nodered-credentials",
-    //         settings: "nodered-settings",
-    //         sessions: "nodered-sessions"
-    //     }
-    // },// enables global context
+    httpNodeCors: {
+        origin: "*",
+        methods: "GET,PUT,POST,DELETE,OPTIONS",
+        preflightContinue: true
+    },
 };
 // Initialise the runtime with a server and settings
 RED.init(server,settings);
@@ -47,41 +47,34 @@ app.use(settings.httpAdminRoot,RED.httpAdmin);
 
 // Serve the http nodes UI from /api
 app.use(settings.httpNodeRoot,RED.httpNode);
-
+app.use(livereload())
 app.get('/nodered/:token/:flowid', (req, res) => {
     // console.log(req.params.token)
     // console.log(req.params.flowid)
     req.session.token = req.params.token;
     req.session.flowid = req.params.flowid;
-    res.redirect('/red');
+    req.session.save()
+    console.log(" get "+ req.sessionID)
+
+    req.session.save(function(err) {
+        // session saved
+       return res.redirect('/red');
+    })
+
 });
 app.post('/nodered', (req, res) => {
     // console.log(req.header.Authorization)
     console.log(req.get('authorization'))
     req.session.token = req.get('authorization');
     res.redirect('/red');
+    // /Qua
 
 });
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-// app.post('/form', (req, res) => {
-//     console.log("SSs")
-//     var username = req.body.uname;
-//     var password = req.body.psw;
-//     if (username==='admin'&&password==='admin'){
-//         req.session.user = "admin";
-//         console.log("main"+req.session.id)
-//
-//
-//         res.redirect('/red');
-//     }
-//     else {
-//         res.redirect('/');
-//     }
-// });
+
 server.listen(8000,"0.0.0.0");
 
-// Start the runtime
 RED.start();
